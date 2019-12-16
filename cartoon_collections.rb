eda3cffb96771a7041bbf1aa4564add624646485
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-16 23:17:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+[i]= 0
+  puts =["Doc", "Dopey", "Bashful","Grumpy"]
+  dwarves.each_with_index
 end
 
 def summon_captain_planet# code an argument here
